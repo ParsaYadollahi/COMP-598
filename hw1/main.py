97839235c258a3bb13dbdbe0a8c52b859b23de2a
Parsa Yadollahi <pyadollahi@coveo.com>
2021-09-17 16:25:16 -0400

    small fixes

--- hw1/main.py
+++ hw1/main.py
@@ -1,11 +1,9 @@
-from os import sep, stat
 import pandas as pd
 
 # Data collection
 df = pd.read_csv("./data.tsv", nrows=10000)
 df = df.loc[df['language'] == "English"]
-df = df.loc[df['content'].str.contains('?', regex=False) == True]
-
+df = df[df['content'].str.contains('?', regex=False) == False]
 
 # Data annotation
 trump_mention = df['content'].str.contains("(?<!\w)Trump(?!\w)", regex=True)
@@ -16,7 +14,6 @@ df = df.assign(trump_mention=trump_mention)
 #Analysis
 number_trump_mention = len(df.loc[df['trump_mention'].str.contains('T', regex=False) == True])
 stats = number_trump_mention/len(df)
-print("Number of Trump mentions = ", stats)
 
 # Adding tweets to tsv
 ds = df[['tweet_id', 'publish_date', 'content', 'trump_mention']]
