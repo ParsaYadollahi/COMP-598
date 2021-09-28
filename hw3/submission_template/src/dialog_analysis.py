d3838f45de95f2d71db46e667dc884816d2c960f
Parsa Yadollahi <pyadollahi@coveo.com>
2021-09-28 10:20:40 -0400

    Done hw3

---
+++ hw3/submission_template/src/dialog_analysis.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import json as js
+import sys
+import argparse as ap
+import os
+
+# if __name__ == '__main__':
+#   parser = ap.ArgumentParser()
+#   parser.add_argument(
+#     '-o',
+#     nargs=1,
+#     default = os.getcwd()
+#   )
+
+#   args = parser.parse_args()
+#   output_file = args.o
+
+
+pony_names = ['twilight sparkle', 'applejack', 'rarity',
+            'pinkie pie', 'rainbow dash', 'fluttershy']
+
+output_file = sys.argv[2]
+input_file = sys.argv[3]
+
+# "/Users/pyadollahicoveo.com/dev/school/COMP-598/hw3/submission_template/data/clean_dialog.csv"
+dt = pd.read_csv(input_file, delimiter=",")
+dt = dt[['pony']]
+
+counts = {name: 0 for name in pony_names}
+verbosity = {name: 0 for name in pony_names}
+tot_verb = 0
+
+for pony in dt['pony']:
+  if pony.lower() in pony_names:
+    counts[pony.lower()] += 1
+  tot_verb += 1
+
+for pony in verbosity:
+  verbosity[pony] = round(counts[pony] / tot_verb, 2)
+
+# /Users/pyadollahicoveo.com/dev/school/COMP-598/hw3/submission_template/src/output.json
+with open(output_file, 'w', encoding='utf-8') as f:
+  js.dump({'counts  ': counts, 'verbosity': verbosity}, f, indent = 4)
---
+++ hw3/submission_template/src/stats.sh
@@ -0,0 +1,13 @@
+# #!/bin/bash
+
+# The number of lines in the file
+wc -l < $1 | sed 's/ //g'
+
+# The first line of the file (i.e., the header row)
+sed -n '1p' < $1
+
+# The number of lines in the last 10,000 rows of the file that contain the string “potus” (case-insensitive).
+tail -10000 $1 | grep -c -in "potus"
+
+# Of rows 100 – 200 (inclusive), how many of them that contain the word “fake”
+sed -n '0,200000p' < $1 | grep -c "fake"
