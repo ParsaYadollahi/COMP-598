f909eb46928b28ff9f41496c02b97e6f7bb63943
pyadollahi <pyadollahi@coveo.com>
2021-10-13 18:32:50 -0400

    Rerouting works

--- hw4/submission_template/src/auth.js
+++ hw4/submission_template/src/auth.js
@@ -4,16 +4,13 @@ function login() {
     if (user != 'nyc' && pass != 'iheartnyc') {
         alert('Invalid user or pass');
     } else {
-        alert(
-            'Thank You for Login & You are Redirecting to Campuslife Website'
-        );
         //Redirecting to other page or webste code or you can set your own html page.
-        window.history = 'https://www.campuslife.co.in';
-        window.history.pushState(
-            { html: response.html, pageTitle: response.pageTitle },
-            '',
-            '/nyc_dash'
-        );
+        window.history.pushState(null, null, '/nyc_dash');
+        // window.history.pushState(
+        //     { html: response.html, pageTitle: response.pageTitle },
+        //     '',
+        //     '/nyc_dash'
+        // );
     }
 }
 //Reset Inputfield code.
