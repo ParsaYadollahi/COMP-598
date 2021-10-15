44a9a7d48a104801579d2f453dade8f50bcff8b2
pyadollahi <pyadollahi@coveo.com>
2021-10-15 01:27:38 -0400

    data_trimmed

--- hw4/submission_template/main.py
+++ hw4/submission_template/main.py
@@ -13,7 +13,7 @@ def main():
 
   parse_dates = ['created date', 'closed date']
   usecols = ['created date', 'closed date', 'incident zip']
-  df = pd.read_csv(f"{cwd}/data/nyc_311_limit.csv", names=columns, usecols=usecols, parse_dates=parse_dates)
+  df = pd.read_csv(f"{cwd}/data_trimmed.csv.csv", names=columns, usecols=usecols, parse_dates=parse_dates)
   df = clean(df)
   calculate_response_times(df)
 
@@ -35,25 +35,39 @@ def clean(df: DataFrame) -> DataFrame:
   df_copy: DataFrame= df.copy()
 
   # Filter dates to be in 2020
-  date_filter = df_copy['closed date'] > pd.to_datetime(datetime.date(year=2020,month=1,day=1))
-  df_copy: DataFrame = df_copy[date_filter]
+  df_copy.dropna(subset=['incident zip'], inplace=True)
+  df['incident zip'] = df['incident zip'].astype(int).astype(str)
 
-  # Remove invalid close dates and drop date columns
   df.dropna(subset=['closed date'], inplace=True)
-  # Remove time from date
-  df_copy['closed month'] = pd.to_datetime(df_copy['closed date']).dt.normalize()
-  df_copy: DataFrame = df_copy.drop('created date', axis=1)
-  df_copy: DataFrame = df_copy.drop('closed date', axis=1)
+  df = df[df['created date'] < df['closed date']]
 
-  # Calculate response times and write to file
-  df_copy['response time'] = (df['closed date'] -  df['created date']).astype('timedelta64[h]')
-  df_copy.to_csv(f'{cwd}/data/response_times.csv', index=False)
+  df['closed month'] = [x.replace(second=0, minute=0, hour=0, day=1, year=2020) for x in df['closed date']]
+  df['closed month'] = df['closed month']
+  df['response time'] = (df['closed date'] - df['created date']).astype('timedelta64[h]')
+
+
+
+
+
+  # date_filter = df_copy['closed date'] > pd.to_datetime(datetime.date(year=2020,month=1,day=1))
+  # df_copy: DataFrame = df_copy[date_filter]
+
+  # # Remove invalid close dates and drop date columns
+  # df.dropna(subset=['closed date'], inplace=True)
+  # # Remove time from date
+  # df_copy['closed month'] = pd.to_datetime(df_copy['closed date']).dt.normalize()
+  # df_copy: DataFrame = df_copy.drop('created date', axis=1)
+  # df_copy: DataFrame = df_copy.drop('closed date', axis=1)
+
+  # # Calculate response times and write to file
+  # df_copy['response time'] = (df['closed date'] -  df['created date']).astype('timedelta64[h]')
+  # df_copy.to_csv(f'{cwd}/data/response_times.csv', index=False)
 
   return df_copy
 
 def calculate_response_times(df: DataFrame) -> None:
-  response_by_month: DataFrame = df.drop('incident zip', axis='columns').groupby('closed month').mean()
-  response_by_zip: DataFrame = df.groupby(['incident zip', 'closed month' ]).mean()
+  response_by_month: DataFrame = df.drop('incident zip', axis=1).groupby('closed month').mean().reset_index()
+  response_by_zip: DataFrame = df.groupby(['incident zip', 'closed month' ]).mean().reset_index()
 
   response_by_month.to_csv(f"{cwd}/data/response_by_month.csv")
   response_by_zip.to_csv(f"{cwd}/data/response_by_zip.csv")
