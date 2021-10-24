49ac7452b84c4dcaf0b7ed003179349796238715
pyadollahi <pyadollahi@coveo.com>
2021-10-24 01:05:56 -0400

    parsing is a bish

--- hw5/submission_template/src/clean.py
+++ hw5/submission_template/src/clean.py
@@ -27,27 +27,60 @@ def clean() -> None:
       json_list.remove(x)
 
   # 2 Rename all title_text to title
-  for x in json_list:
+  for idx, x in enumerate(json_list):
     if 'title_text' in x:
-      x['title'] = x.pop('title_text')
+      title = x.pop('title_text')
+      k = {'title': title}
+      k.update(x)
+      json_list[idx] = k
 
 
-  # 3 standardize times
+  # 3 - 4 standardize times
   for x in json_list:
     if dp.parse(x['createdAt']) is None:
       json_list.remove(x)
     else:
       x['createdAt'] = pytz.UTC.normalize(dateutil.parser.parse(x['createdAt']))
 
-  print()
-  for el in json_list:
-    print(el)
+  # 6 remove all post with invalid authors
+  for x in json_list:
+    if 'author' in x and (x['author'] == "N/A" or x['author'] == None):
+      json_list.remove(x)
 
 
+  # 7- 8 total count clean
+  for x in json_list:
+    if 'total_count' not in x or type(x['total_count']) == int:
+      continue
+    if type(x['total_count']) != int and type(x['total_count']) != float and type(x['total_count']) != str:
+      json_list.remove(x)
 
+    if type(x['total_count']) == float:
+      x['total_count'] = int(x['total_count'])
+      continue
 
+    if (x['total_count'].startswith('-') and x['total_count'][1:].isdigit()) or x['total_count'].isdigit():
+      x['total_count'] = int(x['total_count'])
+    else:
+      json_list.remove(x)
 
+  # 9 tags
+  for x in json_list:
+    if 'tags' not in x:
+      continue
+    # temp tag
+    new_tags = []
+    for tag in x['tags']:
+      if ' ' not in tag:
+        new_tags.append(tag)
+      else:
+        new_tags.extend(tag.split())
+    # replace the current tags
+    x['tags'] = new_tags
 
+  with open(output_file, "w") as f:
+    f.write(json.dumps(json_list, indent=2, default=str))
+  f.close()
 
 
 if __name__ == '__main__':
