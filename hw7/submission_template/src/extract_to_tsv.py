ceba5162d91033cf6bfde481ff4f697105a9c639
pyadollahi <pyadollahi@coveo.com>
2021-11-09 00:49:11 -0500

    A7 - Q2 - done

--- hw7/submission_template/src/extract_to_tsv.py
+++ hw7/submission_template/src/extract_to_tsv.py
@@ -1,39 +1,59 @@
 import argparse
 import os
 import random
+import json
+import sys
+
+def main():
+  ##### ARGPARSE ###### ??????
+  # parser = argparse.ArgumentParser()
+  # parser.add_argument('-o', type=str)
+  # parser.add_argument('-json_file', type=str)
+  # parser.add_argument('-num_posts_to_output', type=str)
+
+  # args = parser.parse_args()
+  # num_posts_to_output = int(args.num_posts_to_output)
+  # json_file = args.json_file
+  # output_file = args.o
 
 
-if __name__ == '__main__':
+  output_file = sys.argv[2]
+  json_file = sys.argv[3]
+  num_posts_to_output = int(sys.argv[4])
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-o', type=str)
-  parser.add_argument('-json_file', type=str)
-  parser.add_argument('-num_posts_to_output', type=str)
-
-  args = parser.parse_args()
-  num_posts_to_output = int(args.num_posts_to_output)
-  json_file = args.json_file
-  output_json = args.o
-
-  path_to_file = os.path.split(os.path.abspath(output_json))[0]
+  # Check if file exists
+  path_to_file = os.path.split(os.path.abspath(output_file))[0]
   if (not os.path.isdir(path_to_file)):
     os.makedirs(path_to_file)
 
+  # Count numb lines
   num_lines = sum(1 for line in open(json_file, 'r'))
-
   assert num_posts_to_output > 0
-  random_lines = random.sample(range(1, num_lines+1), num_posts_to_output)
+
+  # add every line
+  if num_posts_to_output >= num_lines:
+    num_posts_to_output = num_lines - 1
+
+  # Calculate unique random lines
+  random_lines = random.sample(range(0, num_lines), num_posts_to_output)
   random_lines.sort()
-  posts = []
 
   f = open(json_file, 'r')
-
   json_lines = f.readlines()
+
+  # Add random lines to posts
   posts = []
-
-  print(random_lines)
-
   for line_number in random_lines:
-    posts.append(json_lines[line_number])
+    posts.append(json.loads(json_lines[line_number]))
 
-  print(posts)
+  # Write data to output file
+  output_file = open(output_file, 'w')
+  header = 'Name\ttitle\tcoding\n'
+  output_file.write(header)
+
+  for post in posts:
+    out_line = post['data']['name'] + '\t' + post['data']['title'] + '\t' + '\n'
+    output_file.write(out_line)
+
+if __name__ == '__main__':
+  main()
