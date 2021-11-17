34daa929fe54c6941d915392c639ed2a30a40a20
pyadollahi <pyadollahi@coveo.com>
2021-11-16 20:12:53 -0500

    A8

---
+++ hw8/submission_template/src/compile_word_counts.py
@@ -0,0 +1,72 @@
+import argparse
+import pandas as pd
+import json
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-o', help='The output file of this script.', required=True)
+  parser.add_argument('-d', help='The dialog file of this script.', required=True)
+
+  args = parser.parse_args()
+  out_file = args.o
+  in_file = args.d
+
+  stopwords_path = './../data/stopwords.txt'
+  stopwords = open(stopwords_path).read().splitlines()
+
+  ponies_json = {
+        'twilight sparkle': 0,
+        'applejack': 0,
+        'rainbow dash': 0,
+        'rarity': 0,
+        'pinkie pie': 0,
+        'fluttershy': 0
+    }
+
+  # Remove punctuation and lower words
+  df = preprocess_dialogues(in_file)
+  output: dict = {}
+
+  for i in range(len(df['pony'])):
+    name = str(df.iloc[i, 0])
+
+    # init keys of dict
+    if name not in ponies_json:
+      continue
+    if name not in output:
+      output[name] = {}
+
+    # Count number of words
+    for word in df.iloc[i,1].split():
+      if word not in stopwords and word.isalpha():
+        if word not in output[name]:
+          output[name][word] = 1
+        else:
+          output[name][word] += 1
+
+  # Remove elements with less than 5 words
+  for (k,v) in output.copy().items():
+    for (i,j) in v.copy().items():
+      if j < 5:
+        del v[i]
+
+  with open(out_file, 'w') as o:
+    json.dump(output, o, indent=4, sort_keys=True)
+
+
+def preprocess_dialogues(f):
+  with open(f, 'r') as f:
+    df = pd.read_csv(f)
+    df = df[['pony', 'dialog']]
+
+    df['dialog'] = df['dialog'].str.replace('<U+.*>', ' ', regex=True).replace('[()\[\],-.?!;:#&]', ' ', regex=True)
+    df.dropna(how='any', inplace=True)
+
+    for i in range(len(df['pony'])):
+      df.iloc[i, 0] = df.iloc[i, 0].lower()
+      df.iloc[i, 1] = df.iloc[i, 1].lower()
+
+    return df
+
+if __name__ == '__main__':
+  main()
---
+++ hw8/submission_template/src/compute_pony_lang.py
@@ -0,0 +1,75 @@
+import argparse
+import json
+import math
+import os.path as osp
+from collections import Counter
+import re
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-c', help='The input file of this script.', required=True)
+  parser.add_argument('-n', help='The number of words.', required=True, type=int)
+
+  args = parser.parse_args()
+  in_file = args.c
+  num_words = args.n
+
+  if not osp.isabs(in_file):
+        in_file = osp.abspath(in_file)
+
+  tfdif_ponies = {}
+  ponies_json = {
+        'twilight sparkle': 0,
+        'applejack': 0,
+        'rainbow dash': 0,
+        'rarity': 0,
+        'pinkie pie': 0,
+        'fluttershy': 0
+    }
+
+  with open(in_file, 'r') as f:
+    data = json.load(f)
+
+    for (pony, words) in data.items():
+      if pony not in ponies_json:
+        continue
+      tfdif_ponies[pony] = {}
+      for (word, num_times_pony_uses_word) in words.items():
+        tf = num_times_pony_uses_word
+        idf = compute_idf(data, word)
+
+        tfdif_ponies[pony][word] = tf * idf
+
+  return_tfidf = {}
+  for (pony, words) in tfdif_ponies.items():
+    return_tfidf[pony] = [(w) for w, c in Counter(words).most_common(num_words)]
+
+  output = json.dumps(return_tfidf, indent=4)
+  output2 = re.sub(r'": \[\s+', '": [', output)
+  output3 = re.sub(r'",\s+', '", ', output2)
+  output4 = re.sub(r'"\s+\]', '"]', output3)
+  print(output4)
+
+
+
+
+
+
+
+
+
+def compute_idf(data: dict, word: str):
+  total_number_of_ponies = len(data.keys())
+  number_of_ponies_that_use_word = 0
+
+  for (k,v) in data.items():
+    if word in v:
+      number_of_ponies_that_use_word += 1
+
+  assert number_of_ponies_that_use_word != 0
+
+  return math.log(float(total_number_of_ponies / number_of_ponies_that_use_word))
+
+
+if __name__ == '__main__':
+  main()
