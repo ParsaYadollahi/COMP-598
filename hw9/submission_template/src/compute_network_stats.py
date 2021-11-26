c0ad9c35131fa80f2fc919282faf8cd57bc78635
pyadollahi <pyadollahi@coveo.com>
2021-11-26 14:22:34 -0500

    formatting json is a bitch

--- hw9/submission_template/src/compute_network_stats.py
+++ hw9/submission_template/src/compute_network_stats.py
@@ -2,6 +2,7 @@ import argparse
 from io import TextIOWrapper
 import json
 import networkx as nx
+from networkx.algorithms.centrality.betweenness import betweenness_centrality
 from networkx.classes.graph import Graph
 
 def main():
@@ -22,12 +23,25 @@ def main():
       G.add_edge(character, inter_char, weight=num_inter)
   print(G)
 
+
+  f = open(out_file, "w")
+
   # Q1 - most_connected_by_num
   top_connected_edges = {}
   for node in G.nodes():
     top_connected_edges[node] = G.degree(node)
   top_connected_edges = sorted(top_connected_edges, key=top_connected_edges.get, reverse=True)[:3]
   print(top_connected_edges)
+  f.write('{\n')
+
+  f.write("\t\"most_connected_by_num\": [")
+  for x in top_connected_edges:
+    f.write('"%s"' %x)
+    if (x == top_connected_edges[-1]):
+      break
+    f.write(', ')
+
+  f.write('],\n')
 
 
   # Q2 - most_connected_by_weight
@@ -37,6 +51,20 @@ def main():
       top_connected_by_weight[node] += weight['weight']
   top_connected_by_weight = sorted(top_connected_by_weight, key=top_connected_by_weight.get, reverse=True)[:3]
   print(top_connected_by_weight)
+
+  f.write("\t\"most_connected_by_weight\": [")
+  for x in top_connected_by_weight:
+    f.write('"%s"' %x)
+    if (x == top_connected_by_weight[-1]):
+      break
+    f.write(', ')
+
+  f.write('],\n')
+
+  f.write('\n}')
+
+
+  print(betweenness_centrality(G))
 
 
 if __name__ == '__main__':
